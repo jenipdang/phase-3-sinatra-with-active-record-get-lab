9b4e8c2a02cc2bcc78790a5530ac7dc9a538c328
Jeni Dang <jenipdang@yahoo.com>
2022-03-09 20:53:11 -0800

    commit tree 00c92b468d9c7cdba52e6aaa9221b93e5b7294c0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,35 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    # look up the bakery in the database using its ID
+    bakeries = Bakery.find(params[:id])
+    # send them back as a JSON array
+    bakeries.to_json(only: [:id, :name], include: { baked_goods: {only: [:name, :price]}})
+  end
+
+  get '/baked_goods/by_price' do
+    # get all the baked goods sort by desc order
+    # baked_goods = BakedGood.all.order(:price).reverse_order
+    baked_goods = BakedGood.all.reorder('price DESC')
+    # send a JSON-formatted response of the baked goods data with selective attributes
+    baked_goods.to_json(only: [:id, :name, :price])
+  end
+
+  get '/baked_goods/most_expensive' do
+    # look up the baked goods in the database using price
+    most_expensive = BakedGood.all.max_by(&:price)
+    # send them back as a JSON array
+    most_expensive.to_json(only: [:id, :name, :price])
+  end
 
 end
